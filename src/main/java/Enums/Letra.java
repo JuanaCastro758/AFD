b1dd0063199bbcf9c51de127660ea63d01143cf0
JuanaCastro758 <juanaazucenacastrojimenez@gmail.com>
2021-10-07 02:24:50 -0600

    Clase enum para las letras

---
+++ src/main/java/Enums/Letra.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enums;
+
+/**
+ *
+ * @author Chiko malo
+ */
+public enum Letra {
+    A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
+}
